0bb8f02cdad8651cd94e746d79a87438929d33de
Lukáš Vokráčko <lukas.vokracko@gmail.com>
2014-04-12 19:33:02 +0200

    Ukončení

--- src/server/game.cpp
+++ src/server/game.cpp
@@ -1,16 +1,85 @@
 #include "game.h"
 
-Game::Game(float timeout, int map)
+Game::Game(float timeout, int map_id)
 {
 	id = Server::get_instance()->get_game_id();
 	this->timeout = timeout;
+	running = true;
+	map = new Map(map_id);
 	// this->map = Server::get_instance()->get_map(map);
 }
 
 Game::~Game()
 {
-	std::cout << "game~players"<<std::endl;
-	players->clear();
+	for(std::vector<Player *>::iterator it = players.begin(); it != players.end(); ++it)
+	{
+		Server::get_instance()->add_orphan(*it);
+		(*it)->work();
+		players.erase(it);
+	}
+
+	delete map;
+}
+
+bool Game::is_running()
+{
+	return running;
+}
+
+void Game::stop()
+{
+	//std::chrono::duration<int> elapsed_seconds = std::chrono::system_clock::now() - start;
+	//TODO poslat čas hry a další bullshity
+	//TODO elapsed_seconds.count()
+
+	running = false;
+
+	for(std::vector<Player*>::iterator it = players.begin(); it != players.end(); ++it)
+	{
+		(*it)->send_quit();
+	}
+
+}
+
+void Game::set(int player_id, std::string * command)
+{
+
+	//TODO
+	if(*command == "left")
+	{
+		turn_left(player_id);
+	}
+	else if(*command == "right")
+	{
+		turn_right(player_id);
+	}
+	else if(*command == "take")
+	{
+
+	}
+	else if(*command == "open")
+	{
+
+	}
+	else if(*command == "go")
+	{
+
+	}
+	else if(*command == "stop")
+	{
+
+	}
+}
+
+void Game::turn_left(int player_id)
+{
+	// TODO
+	// map->
+}
+
+void Game::turn_right(int player_id)
+{
+	// TODO
 }
 
 /**
@@ -19,12 +88,10 @@ Game::~Game()
  * \param exclude odkaz na hráče, který zprávu vytvořil
  * \param message obsah zprávy
  */
-void Game::send(Player * exclude, std::string message)
+void Game::send(std::string message)
 {
-	for(std::vector<Player*>::iterator it = players->begin(); it != players->end(); ++it)
+	for(std::vector<Player*>::iterator it = players.begin(); it != players.end(); ++it)
 	{
-		if((*it) == exclude) continue;
-
 		(*it)->send(&message);
 	}
 }
@@ -38,26 +105,67 @@ void Game::send(Player * exclude, std::string message)
  */
 bool Game::add_player(Player * p)
 {
-	if(players->size() < 4)
+	if(players.size() < 4)
 	{
-		players->push_back(p);
-		//TODO posílat mapu
+		players.push_back(p);
+		set_color(p);
+		map->emplace_player(p);
 		return true;
 	}
 
 	return false;
 }
 
-std::string Game::to_string()
+Map * Game::get_map()
 {
-	std::string message;
-
-	message.append(players->size() + " ");
-	return message;
-	//TODO informace o mapě
+	return map;
 }
 
-//TODO mazat klienty
+void Game::remove_player(Player * p)
+{
+	if(running)
+	{
+		for(std::vector<Player*>::iterator it = players.begin(); it != players.end(); ++it)
+		{
+			if(*it == p)
+			{
+				players.erase(it);
+				remove_color(p);
+				//TODO unemplace player
+				break;
+			}
+		}
+	}
+}
+
+void Game::remove_color(Player * p)
+{
+	int color = p->get_color();
+	colors[color] = false;
+}
+
+void Game::set_color(Player * p)
+{
+	for(int i = 0; i < 4; ++i)
+	{
+		if(colors[i] == false)
+		{
+			p->set_color(i);
+			colors[i] = true;
+			break;
+		}
+	}
+}
+
+std::string Game::to_string()
+{
+	std::string message = get_id() + ": ";
+	//TODO trvání hry
+	// message.append(players.size() + " ");
+	message.append(players.size() + "/4");
+
+	return message;
+}
 
 int Game::get_id()
 {
--- src/server/game.h
+++ src/server/game.h
@@ -4,35 +4,50 @@
  * \author Lukáš Vokráčko (xvokra00)
  */
 
+class Game;
+
 #pragma once
 
 #include <string>
 #include <vector>
 #include <iostream>
+#include <chrono>
 #include "player.h"
-// #include "map.h" //TODO
+#include "map.h"
 
-class Player;
 class Game
 {
 	private:
-		std::vector<Player*> * players;
+		std::vector<Player*> players;
+		Map * map;
+
 		int id;
-		int map;
+		int map_id;
 		float timeout;
-		// Map * map; //TODO
+		bool running;
+		bool colors[4] = {false};
+
+		std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
 
 	public:
 
 
 	private:
+		void turn_right(int player_id);
+		void turn_left(int player_id);
 
 	public:
-		Game(float timeout, int map);
+		Game(float timeout, int map_id);
 		~Game();
-		void send(Player * exclude, std::string message);
+		void send(std::string message);
 		bool add_player(Player * p);
 		int get_id();
 		std::string to_string();
-
+		bool is_running();
+		void remove_player(Player * p);
+		void set(int player_id, std::string * command);
+		void remove_color(Player * p);
+		void set_color(Player * p);
+		Map * get_map();
+		void stop();
 };
